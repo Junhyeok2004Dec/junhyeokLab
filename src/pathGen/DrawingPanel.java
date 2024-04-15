59741fec47c5a82a74a183e4f55180328d340824
J2DCB <junhyeok1209@outlook.kr>
2024-04-15 21:21:57 +0900

    v21

--- src/pathGen/DrawingPanel.java
+++ src/pathGen/DrawingPanel.java
@@ -1,4 +1,4 @@
-package main;
+package pathGen;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
--- src/pathGen/DrawingProgram.java
+++ src/pathGen/DrawingProgram.java
@@ -7,7 +7,7 @@ public class DrawingProgram {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Drawing Program");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(new main.DrawingPanel());
+		frame.getContentPane().add(new DrawingPanel());
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
